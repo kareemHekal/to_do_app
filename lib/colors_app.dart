c6049a2ee159ad18b34a2a6cf1ca987476eaeba4
East-Sound <kareemhekal19@gmail.com>
2024-08-17 22:18:01 +0300

    updates

--- lib/colors_app.dart
+++ lib/colors_app.dart
@@ -5,6 +5,7 @@ class app_colors {
   static const white = Color(0xffFFFFFF);
   static const black = Color(0xff363636);
   static const grey = Color(0xffC8C9CB);
+  static const green = Color(0xff61E757);
   static const navBarDarkColor = Color(0xff141922);
   static const backGroundColorLigthTheme = Color(0xffDFECDB);
   static const backGroundColorDarkTheme = Color(0xff060E1E);
